eaab2f543c8c5f84d0c6b66220d1e0b15abaf04f
sridivya piratla <sridivya.piratla@genixtec.com>
2024-09-25 16:24:16 +0530

    impl

--- src/main/java/gini/ginidashboardservice/mapper/ClientProfileMapper.java
+++ src/main/java/gini/ginidashboardservice/mapper/ClientProfileMapper.java
@@ -8,13 +8,13 @@ import org.mapstruct.MappingTarget;
 
 import java.util.Map;
 
-@Mapper(componentModel = "spring")
+//@Mapper(componentModel = "spring")
 public interface ClientProfileMapper {
     ClientProfile toEntity(ClientProfileDTO dto);
 
     ClientProfileDTO toDto(ClientProfile entity);
 
-    void updateClientProfileFromDto(ClientProfileDTO dto, @MappingTarget ClientProfile entity);
+    void updateClientProfileFromDto(ClientProfileDTO dto,ClientProfile entity);
 
     default String mapJsonField(Map<String, Object> value) {
         return value != null ? new JsonConverter().convertToJson(value) : null;
