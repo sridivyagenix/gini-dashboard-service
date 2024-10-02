c0c5e94e0f26405b2920151208bfb6c7cf32beed
sridivya piratla <sridivya.piratla@genixtec.com>
2024-10-02 13:34:54 +0530

    people api

--- src/main/java/gini/ginidashboardservice/service/InterventionService.java
+++ src/main/java/gini/ginidashboardservice/service/InterventionService.java
@@ -2,12 +2,17 @@ package gini.ginidashboardservice.service;
 
 import gini.ginidashboardservice.dto.ActivityResponse;
 import gini.ginidashboardservice.dto.InterventionResponse;
+import gini.ginidashboardservice.dto.PeopleDTO;
 import gini.ginidashboardservice.repositories.InterventionRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface InterventionService {
     public Page<InterventionResponse> getInterventionTypeCountsByEmployeeId(Long employeeId, Pageable pageable);
+
+    Page<PeopleDTO> getInteractionsWithAgentNames(Long employeeId,Pageable pageable);
 }
