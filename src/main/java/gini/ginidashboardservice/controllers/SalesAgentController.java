b06cd20c098781f2e052f397c193c2cda78a3f02
sridivya piratla <sridivya.piratla@gmail.com>
2024-10-17 08:44:17 +0530

    people dashboard

---
+++ src/main/java/gini/ginidashboardservice/controllers/SalesAgentController.java
@@ -0,0 +1,84 @@
+package gini.ginidashboardservice.controllers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gini.ginidashboardservice.models.SalesAgent;
+import gini.ginidashboardservice.repositories.SalesAgentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+public class SalesAgentController {
+
+    @Autowired
+    SalesAgentRepository salesAgentRepository;
+
+    @GetMapping("/{id}/performance")
+    public ResponseEntity<Map<String, Object>> getPerformanceMetrics(@PathVariable Long id) {
+        Optional<SalesAgent> salesAgent = salesAgentRepository.findById(id);
+        if (salesAgent.isPresent()) {
+            Map<String, Object> response = new HashMap<>();
+            response.put("performance_segment", salesAgent.get().getPerformanceSegment());
+            response.put("lifetime_value", salesAgent.get().getLifetimeValue());
+            response.put("engagement_score", salesAgent.get().getEngagementScore());
+            response.put("60_day_sentiment", salesAgent.get().getSentiment60Day());
+            response.put("30_day_sales_forecast", salesAgent.get().getSalesForecast30Day());
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping("/{id}/strengths")
+    public ResponseEntity<String[]> getStrengths(@PathVariable Long id) {
+        Optional<SalesAgent> salesAgent = salesAgentRepository.findById(id);
+        if (salesAgent.isPresent()) {
+            String strengths = salesAgent.get().getStrengths();
+            String[] strengthsArray = strengths.split(", ");
+            return ResponseEntity.ok(strengthsArray);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping("/{id}/products")
+    public ResponseEntity<List<Map<String, String>>> getProducts(@PathVariable Long id) {
+        Optional<SalesAgent> salesAgent = salesAgentRepository.findById(id);
+        if (salesAgent.isPresent()) {
+            String productsJson = salesAgent.get().getProducts();
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                List<Map<String, String>> productsList = mapper.readValue(productsJson, (new TypeReference<List<Map<String, String>>>() {
+                }));
+                return ResponseEntity.ok(productsList);
+            } catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping("/{id}/topics")
+    public ResponseEntity<String[]> getTopics(@PathVariable Long id) {
+        Optional<SalesAgent> salesAgent = salesAgentRepository.findById(id);
+        if (salesAgent.isPresent()) {
+            String topics = salesAgent.get().getTopics();
+            String[] topicsArray = topics.split(", ");
+            return ResponseEntity.ok(topicsArray);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+}
--- src/main/java/gini/ginidashboardservice/models/SalesAgent.java
+++ src/main/java/gini/ginidashboardservice/models/SalesAgent.java
@@ -9,58 +9,80 @@ import java.sql.Timestamp;
 @Entity
 @Data
 @Table(name = "sales_agents", schema = "axonai_demo")
+
 public class SalesAgent {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sales_agent_id")
-    private Long salesAgentId;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "sales_agent_id")
+        private Long salesAgentId;
 
-    @Column(name = "company_id")
-    private Long companyId;
+        @Column(name = "company_id", nullable = false)
+        private Long companyId;
 
-    @Column(name = "distributor_id")
-    private Long distributorId;
+        @Column(name = "distributor_id")
+        private Long distributorId;
 
-    @Column(name = "agent_name")
-    private String agentName;
+        @Column(name = "agent_name", nullable = false)
+        private String agentName;
 
-    @Column(name = "license_no")
-    private String licenseNo;
+        @Column(name = "license_no", nullable = false)
+        private String licenseNo;
 
-    @Column(name = "agent_type")
-    private String agentType;
+        @Column(name = "agent_type", nullable = false)
+        private String agentType;
 
-    @Column(name = "territory")
-    private String territory;
+        @Column(name = "territory", nullable = false)
+        private String territory;
 
-    @Column(name = "email")
-    private String email;
+        @Column(name = "email")
+        private String email;
 
-    @Column(name = "address")
-    private String address;
+        @Column(name = "address")
+        private String address;
 
-    @Column(name = "phone")
-    private String phone;
+        @Column(name = "phone")
+        private String phone;
 
-    @Column(name = "ms_teams")
-    private String msTeams;
+        @Column(name = "ms_teams")
+        private String msTeams;
 
-    @Column(name = "performance_segment")
-    private String performanceSegment;
+        @Column(name = "performance_segment", nullable = false)
+        private String performanceSegment;
 
-    @Column(name = "prev_year")
-    private Integer prevYear;
+        @Column(name = "prev_year")
+        private Integer prevYear;
 
-    @Column(name = "prev_year_sales_amount")
-    private BigDecimal prevYearSalesAmount;
+        @Column(name = "prev_year_sales_amount")
+        private BigDecimal prevYearSalesAmount;
 
-    @Column(name = "created_at")
-    private Timestamp createdAt;
+        @Column(name = "lifetime_value")
+        private BigDecimal lifetimeValue;
 
-    @Column(name = "last_modified_at")
-    private Timestamp lastModifiedAt;
+        @Column(name = "engagement_score")
+        private Integer engagementScore;
 
-    @Column(name = "Column6")
-    private String column6;
+        @Column(name = "60_day_sentiment")
+        private Integer sentiment60Day;
+
+        @Column(name = "30_day_sales_forecast")
+        private BigDecimal salesForecast30Day;
+
+        @Column(name = "strengths")
+        private String strengths;
+
+        @Column(name = "topics")
+        private String topics;
+
+        @Column(name = "products", columnDefinition = "json")
+        private String products;
+
+        @Column(name = "products_str")
+        private String productsStr;
+
+        @Column(name = "created_at", nullable = false, updatable = false)
+        private Timestamp createdAt;
+
+        @Column(name = "last_modified_at", nullable = false)
+        private Timestamp lastModifiedAt;
 }
